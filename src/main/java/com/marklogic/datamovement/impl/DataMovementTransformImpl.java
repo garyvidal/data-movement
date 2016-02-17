188370ca27441a55458ff929bbbfd79abe928b97
Sam Mefford <sam.mefford@marklogic.com>
2016-02-17 07:53:59 -0700

    fix broken package names

--- src/main/java/com/marklogic/datamovement/impl/DataMovementTransformImpl.java
+++ src/main/java/com/marklogic/datamovement/impl/DataMovementTransformImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.marklogic.datamovement;
+package com.marklogic.datamovement.impl;
 
 import com.marklogic.datamovement.DataMovementTransform;
 import java.util.ArrayList;
